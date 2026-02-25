0636bf085e70e2a51faa44b4386172cd59bba8a1
jlucasbx <jlucasb16@gmail.com>
2026-02-25 17:01:54 -0300

    refactor: remove deprecated UploadFileValidator in favor of UploadFileDtoValidator

--- src/infrastructure/http/validators/index.ts
+++ src/infrastructure/http/validators/index.ts
@@ -1,2 +1,1 @@
-export { UploadFileValidator } from "@/infrastructure/http/validators/upload-file.validator";
 export { UploadFileDtoValidator } from "@/infrastructure/http/validators/upload-file-dto.validator";
